b4a7b4802eb2c6508749d7e45ea78ec01868bf49
hansangsu <hsangsu40@gmail.com>
2021-04-09 18:28:54 +0900

    푸시

--- Assets/Scripts/Prawn.cs
+++ Assets/Scripts/Prawn.cs
@@ -7,6 +7,7 @@ using System;
 public class SaveData
 {
     public List<Prawn> prawns = new List<Prawn>();
+    public bool isFirstStart = false;
 }
 
 [Serializable]
@@ -20,12 +21,13 @@ public class Prawn
     public int price;
     public int foodAmount;
     public float restTime;
+    public float currentRestTime;
 
     public string name;
     public string ex;
     public Sprite spr;
 
-    public Prawn(short id, int hp, int mental, int str, int def, int price, int foodAmount, float restTime,string name,string ex, Sprite spr)
+    public Prawn(short id, int hp, int mental, int str, int def, int price, int foodAmount, float restTime,float currentRestTime,string name,string ex, Sprite spr)
     {
         this.id = id;
         this.hp = hp;
@@ -35,6 +37,7 @@ public class Prawn
         this.price = price;
         this.foodAmount = foodAmount;
         this.restTime = restTime;
+        this.currentRestTime = currentRestTime;
         this.name = name;
         this.ex = ex;
         this.spr = spr;
