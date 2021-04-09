183dfeba17b63b210132ad84591baa4d32c31729
hansangsu <hsangsu40@gmail.com>
2021-04-09 12:53:28 +0900

    수정

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -8,8 +8,39 @@ using System;
 
 public class GameManager : MonoSingleton<GameManager>
 {
+    [SerializeField] SaveData saveData;
+    public SaveData savedData { get; }
+
     private void Awake()
     {
+        Application.runInBackground = true;
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+        saveData = new SaveData();
+    }
+
+    public void Save()
+    {
+
+    }
+
+    public void Load()
+    {
         
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnApplicationQuit()
+    {
+        Save();
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        Save();
     }
 }
