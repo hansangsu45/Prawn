7217c51693ef305fa962356368814d322b3f9b7d
sdy362 <seodye04@gmail.com>
2021-04-21 23:28:09 +0900

    물고기 움직이는 코드 추가 (아트가 나오면 작업 게속 함)

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    private FishPooling fishPooling;
+
+    private Vector2 targetPosition;
+
+    private float speed = 0f;
+
+    [SerializeField] private float minSpeed;
+    [SerializeField] private float maxSpeed;
+
+    private void Awake()
+    {
+        fishPooling = FindObjectOfType<FishPooling>();
+    }
+
+    private void Update()
+    {
+        if (transform.localPosition.x <= GameManager.Instance.fishMinPosition.x)
+        {
+            speed = Random.Range(minSpeed, maxSpeed);
+        }
+
+        targetPosition = transform.localPosition;
+        targetPosition.x += speed * Time.deltaTime;
+        transform.localPosition = targetPosition;
+
+        if (transform.localPosition.x >= GameManager.Instance.fishMaxPosition.x)
+        {
+            FishDestroy();
+        }
+    }
+
+    public void FishDestroy()
+    {
+        gameObject.SetActive(false);
+        gameObject.transform.SetParent(fishPooling.transform, true);
+    }
+}
--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -30,6 +30,23 @@ public class GameManager : MonoSingleton<GameManager>
     public Image hpImage, mentalImage;
     public Text hpTxt, mentalTxt, systemTxt;
 
+    #region 물고기 움직임 관련 변수
+    //랜덤 위치 (maxPosition minPosition) 제한
+    //랜덤 초 기다림 (max time min time) 제한
+    private FishPooling fishPooling;
+
+    [Header("물고기 스폰 관련 변수")]
+    [Tooltip("물고기 스폰 위치 최소 제한")]
+    public Vector2 fishMinPosition;
+    [Tooltip("물고기 스폰 위치 최대 제한")]
+    public Vector2 fishMaxPosition;
+    [Tooltip("물고기 스폰 최소 시간")]
+    [SerializeField] private float fishMinTime;
+    [Tooltip("물고기 스폰 최대 시간")]
+    [SerializeField] private float fishMaxTime;
+
+    #endregion
+
     private void Awake()
     {
         Application.runInBackground = true;
@@ -50,8 +67,25 @@ public class GameManager : MonoSingleton<GameManager>
         }
 
         prawnAnimator = myPrawn.GetComponent<Animator>();
+        fishPooling = FindObjectOfType<FishPooling>();
+
+        #region (혹시 잘못되면 바로 알아볼 수 있도록 코드 추가함) fishPooling 관련 예외 처리 (유니티 안에서만 실행됨)
+#if UNITY_EDITOR
+        if (fishPooling == null) //fishPooling이 없다면 실행
+        {
+            UnityEditor.EditorUtility.DisplayDialog("FishPooling 오류", "FishPooling이 없습니다. FishPooling을 추가해 주세요", "확인");
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else if(fishPooling.transform.childCount <= 0)//fishPooling에 fish가 0개 이하라면 실행
+        {
+            UnityEditor.EditorUtility.DisplayDialog("FishPooling 오류", "FishPooling안에 Fish가 없습니다. Fish를 추가해 주세요", "확인");
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+#endif
+        #endregion
 
         StartCoroutine(FadeEffect(BlackPanel.GetComponent<Image>()));
+        StartCoroutine(SpawnFish());
     }
 
     #region 저장/로드
@@ -203,5 +237,39 @@ public class GameManager : MonoSingleton<GameManager>
         mainObjs[1].SetActive(true);
         systemTxt.text = msg;
         uiObjs.Add(mainObjs[1]);
+    }
+
+    private IEnumerator SpawnFish()
+    {
+        yield return new WaitForSeconds(10f);
+
+        Transform fishTransform = null;
+        GameObject fishGO = null;
+
+        Vector2 randomPosition = Vector2.zero;
+
+        float randomWaitSecond = 0f;
+
+        while (true)
+        {
+            if (fishPooling.transform.childCount > 0)
+            {
+                fishTransform = fishPooling.transform.GetChild(0);
+                fishGO = fishTransform.gameObject;
+            }
+            else
+            {
+                yield return new WaitForSeconds(fishMinTime);
+                continue;
+            }
+
+            fishGO.SetActive(true);
+            randomPosition.x = fishMinPosition.x;
+            randomPosition.y = UnityEngine.Random.Range(fishMinPosition.y, fishMaxPosition.y);
+            fishTransform.position = randomPosition;
+            fishTransform.SetParent(null, true);
+            randomWaitSecond = UnityEngine.Random.Range(fishMinTime, fishMaxTime);
+            yield return new WaitForSeconds(randomWaitSecond);
+        }
     }
 }
